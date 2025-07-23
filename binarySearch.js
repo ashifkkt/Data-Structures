cbabf669e702adb20dbd7d48cf9aa545b49cd8fd
ashifkkt <ashifkkt1010@gmail.com>
2025-07-23 09:19:56 +0530

    added binary Search

---
+++ binarySearch.js
@@ -0,0 +1,23 @@
+function binarySearch(arr,val){
+    let min =0;
+    let max=arr.length -1;
+
+    while(min <= max){
+        let middle=Math.floor((min+max)/2);
+        let currentElement=arr[middle];
+
+        if(currentElement < val){
+            min=middle+1;
+        }
+        else if(currentElement > val){
+            max= middle-1;
+        }
+         else{
+            return middle
+         }
+    }
+    return -1
+}
+
+
+console.log(binarySearch([1,2,3,5,6,8,9],2))
