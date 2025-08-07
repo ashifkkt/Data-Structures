291c1e43d8285c82e1b5ba62d724c524b74f8cca
ashifkkt <ashifkkt1010@gmail.com>
2025-08-07 09:25:47 +0530

    added bubble sort psuedo method code

---
+++ Bubblesortpsuedo.js
@@ -0,0 +1,13 @@
+function bubbleSort(nums){
+    for (let i=nums.length-1;i>=0;i--){
+      for (let j=0;j<=i-1;j++){
+        console.log(nums[j],nums[j+1])
+       if(nums[j] > nums[j+1]){
+        [nums[j],nums[j+1]]=[nums[j+1],nums[j]]
+       }
+      }
+    }
+    return nums
+}
+
+console.log(bubbleSort([10,2,1,6,4,3]))
